4c53ae97e0f6e43a570d6e149af942e6cadc0296
dxdydz <82103357+Shadow56579@users.noreply.github.com>
2021-05-03 15:42:47 +0300

    Update 2_1.c

--- 2/2_1.c
+++ 2/2_1.c
@@ -3,8 +3,6 @@
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
-//led(1,2,3,4) - PB(3,4,5,6)
-//button(1,2,3) - PC(13,14,15)
 
 void delay(uint32_t);
 void view(int8_t);
@@ -15,13 +13,15 @@ int main(void)
 {
 	int8_t number = 0;
 
-	RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOBEN) | (RCC_AHB2ENR_GPIOCEN);
+	RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOBEN) | (RCC_AHB2ENR_GPIODEN);
 	RCC->APB2ENR |= (RCC_APB2ENR_SYSCFGEN);
-	GPIOB->MODER &= ~(GPIO_MODER_MODE3_Msk | GPIO_MODER_MODE4_Msk | GPIO_MODER_MODE5_Msk | GPIO_MODER_MODE6_Msk);
-	GPIOC->MODER &= ~(GPIO_MODER_MODE13_Msk | GPIO_MODER_MODE14_Msk | GPIO_MODER_MODE15_Msk);
-	GPIOB->MODER |= (1 << GPIO_MODER_MODE3_Pos) | (1 << GPIO_MODER_MODE4_Pos) | (1 << GPIO_MODER_MODE5_Pos) | (1 << GPIO_MODER_MODE6_Pos);
-	GPIOC->MODER |= (0 << GPIO_MODER_MODE13_Pos) | (0 << GPIO_MODER_MODE14_Pos) | (0 << GPIO_MODER_MODE15_Pos);
-	SYSCFG->EXTICR[3] = SYSCFG_EXTICR4_EXTI14_PC;
+
+	GPIOD->MODER &= ~(GPIO_MODER_MODE0_Msk | GPIO_MODER_MODE1_Msk | GPIO_MODER_MODE2_Msk | GPIO_MODER_MODE3_Msk);
+	GPIOB->MODER &= ~(GPIO_MODER_MODE14_Msk);
+	GPIOD->MODER |= (1 << GPIO_MODER_MODE0_Pos) | (1 << GPIO_MODER_MODE1_Pos) | (1 << GPIO_MODER_MODE2_Pos) | (1 << GPIO_MODER_MODE3_Pos);
+	GPIOB->MODER |= (0 << GPIO_MODER_MODE14_Pos);
+
+	SYSCFG->EXTICR[3] = SYSCFG_EXTICR4_EXTI14_PB;
 	NVIC_EnableIRQ(EXTI15_10_IRQn);
 	EXTI->IMR1 |= EXTI_IMR1_IM14;
 	EXTI->FTSR1 |= EXTI_FTSR1_FT14;
@@ -34,7 +34,7 @@ int main(void)
 		{
 			uint32_t counter = 0;
 
-			while((GPIOC->IDR & GPIO_IDR_ID14) == 0)
+			while((GPIOB->IDR & GPIO_IDR_ID14) == 0)
 			{
 				counter++;
 				if(counter == 1000000)
@@ -78,6 +78,6 @@ void EXTI15_10_IRQHandler()
 
 void view(int8_t num)
 {
-	GPIOB->ODR &= ~(GPIO_ODR_OD3 | GPIO_ODR_OD4 | GPIO_ODR_OD5 | GPIO_ODR_OD6);
-	GPIOB->ODR |= (num & 0x0F) << 3;
+	GPIOD->ODR &= ~(GPIO_ODR_OD0 | GPIO_ODR_OD1 | GPIO_ODR_OD2 | GPIO_ODR_OD3);
+	GPIOD->ODR |= (num & 0x0F) << 0;
 }
