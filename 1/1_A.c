a94677e3d065524c54d01bc7f0631365813b0bfb
dxdydz <82103357+Shadow56579@users.noreply.github.com>
2021-05-03 17:31:12 +0300

    Update 1_A.c

--- 1/1_A.c
+++ 1/1_A.c
@@ -4,11 +4,9 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 #define delay_default 1000000
-//led(1,2,3,4) - PB(3,4,5,6)
-//button(1,2,3) - PC(13,14,15)
 
 enum Anim{
-	ANIM1_1 = 0,ANIM1_2 = 1,ANIM1_3 = 2,ANIM1_4 = 3,ANIM2_1 = 4,ANIM2_2 = 5
+	ANIM1_1 = 0,ANIM1_2 = 1,ANIM1_3 = 2,ANIM1_4 = 3,ANIM1_5 = 4,ANIM1_6 = 5,ANIM1_7 = 6,ANIM1_8 = 7,ANIM2_1 = 8,ANIM2_2 = 9
 };
 
 void delay(uint32_t);
@@ -18,19 +16,23 @@ int main(void)
 
 	int8_t counter = ANIM1_1;
 
-	RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOBEN) | (RCC_AHB2ENR_GPIOCEN);
-	GPIOC->MODER &= ~(GPIO_MODER_MODE14_Msk);
-	GPIOB->MODER &= ~(GPIO_MODER_MODE3_Msk | GPIO_MODER_MODE4_Msk | GPIO_MODER_MODE5_Msk | GPIO_MODER_MODE6_Msk);
-	GPIOB->MODER |= (1 << GPIO_MODER_MODE3_Pos) | (1 << GPIO_MODER_MODE4_Pos) | (1 << GPIO_MODER_MODE5_Pos) | (1 << GPIO_MODER_MODE6_Pos);
+	RCC->AHB2ENR |= (RCC_AHB2ENR_GPIOBEN) | (RCC_AHB2ENR_GPIODEN);
+
+	GPIOB->MODER &= ~(GPIO_MODER_MODE14_Msk);
+	GPIOD->MODER &= ~(GPIO_MODER_MODE0_Msk | GPIO_MODER_MODE1_Msk | GPIO_MODER_MODE2_Msk | GPIO_MODER_MODE3_Msk |
+			GPIO_MODER_MODE4_Msk | GPIO_MODER_MODE5_Msk | GPIO_MODER_MODE6_Msk | GPIO_MODER_MODE7_Msk);
+	GPIOD->MODER |= (1 << GPIO_MODER_MODE0_Pos) | (1 << GPIO_MODER_MODE1_Pos) | (1 << GPIO_MODER_MODE2_Pos) | (1 << GPIO_MODER_MODE3_Pos)
+			| (1 << GPIO_MODER_MODE4_Pos) | (1 << GPIO_MODER_MODE5_Pos) | (1 << GPIO_MODER_MODE6_Pos) | (1 << GPIO_MODER_MODE7_Pos);
 
 	while(1)
 	{
 
-		GPIOB->BSRR = (GPIO_BSRR_BR3) | (GPIO_BSRR_BR4) | (GPIO_BSRR_BR5) | (GPIO_BSRR_BR6);
+		GPIOD->BSRR = (GPIO_BSRR_BR0) | (GPIO_BSRR_BR1) | (GPIO_BSRR_BR2) | (GPIO_BSRR_BR3) | (GPIO_BSRR_BR4) | (GPIO_BSRR_BR5)
+				| (GPIO_BSRR_BR6) | (GPIO_BSRR_BR7);
 
-		if((GPIOC->IDR & GPIO_IDR_ID14) == 0)
+		if((GPIOB->IDR & GPIO_IDR_ID14) == 0)
 		{
-			if(counter <= ANIM1_4)
+			if(counter <= ANIM1_8)
 			{
 				counter = ANIM2_1;
 			}
@@ -40,49 +42,79 @@ int main(void)
 				counter = ANIM1_1;
 			}
 
-			while((GPIOC->IDR & GPIO_IDR_ID14) == 0);
+			while((GPIOB->IDR & GPIO_IDR_ID14) == 0);
 		}
 
 
 
 		if(counter == ANIM1_1)
 		{
-			GPIOB->BSRR = (GPIO_BSRR_BS3);
+			GPIOD->BSRR = (GPIO_BSRR_BS0);
 			counter = ANIM1_2;
 			delay(delay_default/5);
 		}
 
 		else if(counter == ANIM1_2)
 		{
-			GPIOB->BSRR = (GPIO_BSRR_BS4);
+			GPIOD->BSRR = (GPIO_BSRR_BS1);
 			counter = ANIM1_3;
 			delay(delay_default/5);
 		}
 
 		else if(counter == ANIM1_3)
 		{
-			GPIOB->BSRR = (GPIO_BSRR_BS5);
+			GPIOD->BSRR = (GPIO_BSRR_BS2);
 			counter = ANIM1_4;
 			delay(delay_default/5);
 		}
 
 		else if(counter == ANIM1_4)
 		{
-			GPIOB->BSRR = (GPIO_BSRR_BS6);
+			GPIOD->BSRR = (GPIO_BSRR_BS3);
+			counter = ANIM1_5;
+			delay(delay_default/5);
+		}
+
+		else if(counter == ANIM1_5)
+		{
+			GPIOD->BSRR = (GPIO_BSRR_BS4);
+			counter = ANIM1_6;
+			delay(delay_default/5);
+		}
+
+		else if(counter == ANIM1_6)
+		{
+			GPIOD->BSRR = (GPIO_BSRR_BS5);
+			counter = ANIM1_7;
+			delay(delay_default/5);
+		}
+
+		else if(counter == ANIM1_7)
+		{
+			GPIOD->BSRR = (GPIO_BSRR_BS6);
+			counter = ANIM1_8;
+			delay(delay_default/5);
+		}
+
+		else if(counter == ANIM1_8)
+		{
+			GPIOD->BSRR = (GPIO_BSRR_BS7);
 			counter = ANIM1_1;
 			delay(delay_default/5);
 		}
 
 		else if(counter == ANIM2_1)
 		{
-			GPIOB->BSRR = (GPIO_BSRR_BS3) | (GPIO_BSRR_BS4) | (GPIO_BSRR_BS5) | (GPIO_BSRR_BS6);
+			GPIOD->BSRR = (GPIO_BSRR_BS0) | (GPIO_BSRR_BS1) | (GPIO_BSRR_BS2) | (GPIO_BSRR_BS3) | (GPIO_BSRR_BS4) | (GPIO_BSRR_BS5)
+					| (GPIO_BSRR_BS6) | (GPIO_BSRR_BS7);
 			delay(delay_default/5);
 			counter = ANIM2_2;
 		}
 
 		else
 		{
-			GPIOB->BSRR = (GPIO_BSRR_BR3) | (GPIO_BSRR_BR3) | (GPIO_BSRR_BR4);
+			GPIOD->BSRR = (GPIO_BSRR_BR0) | (GPIO_BSRR_BR1) | (GPIO_BSRR_BR2) | (GPIO_BSRR_BR3) | (GPIO_BSRR_BR4) | (GPIO_BSRR_BR5)
+					| (GPIO_BSRR_BR6) | (GPIO_BSRR_BR7);
 			delay(delay_default/5);
 			counter = ANIM2_1;
 		}
