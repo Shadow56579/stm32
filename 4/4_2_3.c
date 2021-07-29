90c8b24595258bd203b4785193ed9a69102be190
dxdydz <82103357+Shadow56579@users.noreply.github.com>
2021-07-29 18:41:14 +0300

    Update 4_2_3.c

--- 4/4_2_3.c
+++ 4/4_2_3.c
@@ -8,6 +8,7 @@
 uint16_t adc_current_data[2];
 
 void delay(int64_t delay_size);
+void setup_adc_dma( uint16_t *array_to_write_to );
 
 int main(void)
 {
@@ -48,9 +49,14 @@ int main(void)
 
 	// Настроить канал, из которого мы будем считывать данные.
 	// Канал №7,6 , всего каналов - 2
-	ADC2->JSQR |= 7 << ADC_JSQR_JSQ2_Pos
-				  | 6 << ADC_JSQR_JSQ1_Pos
-				  | 1 << ADC_JSQR_JL_Pos;
+	ADC2->SQR1 |= 7 << ADC_SQR1_SQ2_Pos
+				   | 6 << ADC_SQR1_SQ1_Pos
+				   | 1 << ADC_SQR1_L_Pos;
+
+	// Включить работу с DMA в АЦП и настроить DMA.
+	ADC2->CFGR |= ADC_CFGR_DMAEN | ADC_CFGR_DMACFG;
+
+	setup_adc_dma(adc_current_data);
 
 	//Настройка ШИМ
 	TIM3->PSC = 0;
@@ -75,18 +81,11 @@ int main(void)
 
 	while(1)
 	{
-		// Начать считывание данных из АЦП
-		ADC2->CR |= ADC_CR_JADSTART;
+		ADC2->CR |= ADC_CR_ADSTART;
 
-		// Подождать до завершения измерения
-		while ( !(ADC2->ISR & ADC_ISR_JEOS) ){}
+		while ( !(ADC2->ISR & ADC_ISR_EOS) ){}
 
-		// Сбросить флаг завершения измерения
-		ADC2->ISR &= ~ADC_ISR_JEOS;
-		ADC2->ISR &= ~ADC_ISR_JEOC;
-
-		adc_current_data[0] = ADC2->JDR1;
-		adc_current_data[1] = ADC2->JDR2;
+		ADC2->ISR |= ADC_ISR_EOS;
 	}
 
 }
@@ -107,4 +106,31 @@ void TIM2_IRQHandler(void)
 void delay(int64_t delay_size)
 {
 	for(int64_t i;i<delay_size;i++);
+}
+
+void setup_adc_dma( uint16_t *array_to_write_to )
+{
+	// Включить тактирование DMA
+	RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN | RCC_AHB1ENR_DMAMUX1EN;
+
+	// Настроить адрес откуда будет происходить чтение
+	DMA1_Channel1->CPAR = (uint32_t)&(ADC2->DR);
+
+	// Настроить адрес куда будет происходить записью надо напрямую преобразовать
+	// адрес в число типа uin32_t, так как все адреса - 32 значения.
+	DMA1_Channel1->CMAR = (uint32_t)array_to_write_to;
+
+	// Настроить количество значений, которые мы хотим считать.
+	DMA1_Channel1->CNDTR = 2;
+
+	// Настроить канал DMA: размер данных 16 бит источник и приемник, циклический
+	// режи, увеличивать указатель при записи.
+	DMA1_Channel1->CCR |= 1 << DMA_CCR_MSIZE_Pos | 1 << DMA_CCR_PSIZE_Pos
+					   | DMA_CCR_MINC | DMA_CCR_CIRC;
+
+	// Настроить мультиплексор каналов DMA
+	DMAMUX1_Channel0->CCR |= (36 << DMAMUX_CxCR_DMAREQ_ID_Pos);
+
+	// Включить DMA
+	DMA1_Channel1->CCR |= DMA_CCR_EN;
 }
